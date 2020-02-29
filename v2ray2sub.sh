82b7a107e1f6e39516da191597d1836e26cd9d43
root <root@localhost.localdomain>
2020-02-29 14:00:32 +0800

    auto generate output dir

--- v2ray2sub.sh
+++ v2ray2sub.sh
@@ -3,6 +3,10 @@ CONFIG=${SHELL_FOLDER}/config
 OUTPUT_DIR=${SHELL_FOLDER}/output
 OUTPUT=${OUTPUT_DIR}/index.html
 PYTHON_FILE=${SHELL_FOLDER}/v2ray2sub.py 
-. ${CONFIG}
 
-python3 ${PYTHON_FILE} -a $HOST -o ${OUTPUT} ${JSON}
+
+if [ ! -d $OUTPUT_DIR ]; then
+    mkdir $OUTPUT_DIR
+fi
+. $CONFIG
+python3 $PYTHON_FILE -a $HOST -o $OUTPUT $JSON
